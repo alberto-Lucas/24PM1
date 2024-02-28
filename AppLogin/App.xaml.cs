93506bd08d24f6e4e912a44c9942c67f55bd9eae
Lucas Alberto <lucas.alberto@linx.com>
2024-02-28 11:54:35 -0300

    main

---
+++ AppLogin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
